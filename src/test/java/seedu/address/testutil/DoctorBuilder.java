b8db41399d466ba29694660cf5542bc9c54e02ac
leezhanpeng <zhanpenglee@outlook.com>
2023-09-30 19:51:50 +0800

    Add testing for Doctor and Patient class

---
+++ src/test/java/seedu/address/testutil/DoctorBuilder.java
@@ -0,0 +1,96 @@
+package seedu.address.testutil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Doctor;
+import seedu.address.model.person.Phone;
+import seedu.address.model.affiliation.Affiliation;
+import seedu.address.model.util.SampleDataUtil;
+
+/**
+ * A utility class to help with building Doctor objects.
+ */
+public class DoctorBuilder {
+
+    public static final String DEFAULT_NAME = "Jennifer Chue";
+    public static final String DEFAULT_PHONE = "85552222";
+    public static final String DEFAULT_EMAIL = "jenchue@facemail.com";
+    public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+
+    private Name name;
+    private Phone phone;
+    private Email email;
+    private Address address;
+    private Set<Affiliation> affiliations;
+
+    /**
+     * Creates a {@code DoctorBuilder} with the default details.
+     */
+    public DoctorBuilder() {
+        name = new Name(DEFAULT_NAME);
+        phone = new Phone(DEFAULT_PHONE);
+        email = new Email(DEFAULT_EMAIL);
+        address = new Address(DEFAULT_ADDRESS);
+        affiliations = new HashSet<>();
+    }
+
+    /**
+     * Initializes the DoctorBuilder with the data of {@code personToCopy}.
+     */
+    public DoctorBuilder(Doctor doctorToCopy) {
+        name = doctorToCopy.getName();
+        phone = doctorToCopy.getPhone();
+        email = doctorToCopy.getEmail();
+        address = doctorToCopy.getAddress();
+        affiliations = new HashSet<>(doctorToCopy.getAffiliations());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Doctor} that we are building.
+     */
+    public DoctorBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+    /**
+     * Parses the {@code affiliations} into a {@code Set<Affiliation>} and set it to the {@code Doctor} that we are building.
+     */
+    public DoctorBuilder withAffiliations(String ... affiliations) {
+        this.affiliations = SampleDataUtil.getAffiliationSet(affiliations);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Address} of the {@code Doctor} that we are building.
+     */
+    public DoctorBuilder withAddress(String address) {
+        this.address = new Address(address);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code Doctor} that we are building.
+     */
+    public DoctorBuilder withPhone(String phone) {
+        this.phone = new Phone(phone);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Email} of the {@code Doctor} that we are building.
+     */
+    public DoctorBuilder withEmail(String email) {
+        this.email = new Email(email);
+        return this;
+    }
+
+    public Doctor build() {
+        return new Doctor(name, phone, email, address, affiliations);
+    }
+
+}
