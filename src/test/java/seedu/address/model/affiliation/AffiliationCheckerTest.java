a8dd2757e079d27bed0bdfd62bfceb73d517b092
Seonlo99 <Zheng_chenglong1999@hotmail.com>
2023-10-11 20:30:37 +0800

    Add Test for Affiliation Checker

---
+++ src/test/java/seedu/address/model/affiliation/AffiliationCheckerTest.java
@@ -0,0 +1,85 @@
+package seedu.address.model.affiliation;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.affiliation.exceptions.AffiliationPersonNotFoundException;
+import seedu.address.model.affiliation.exceptions.SamePersonAffiliationException;
+import seedu.address.model.affiliation.exceptions.SameRoleAffiliationException;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_AFFILIATION_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ROLE_BOB;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+public class AffiliationCheckerTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AffiliationChecker(null));
+    }
+
+    @Test
+    public void check_withAffiliationPersonNotExist_AffiliationPersonNotFoundException() {
+        Person person = new PersonBuilder()
+                .withName(VALID_NAME_AMY)
+                .withRole(VALID_ROLE_BOB)
+                .withAffiliations(VALID_AFFILIATION_BOB)
+                .build();
+        AffiliationChecker affiliationChecker = new AffiliationChecker(person);
+        assertThrows(AffiliationPersonNotFoundException.class, () -> affiliationChecker.check(model));
+    }
+
+    @Test
+    public void check_withAffiliationPersonSameAsPersonAdding_SamePersonAffiliationException() {
+        String VALID_NAME_ALICE = "Alice Pauline";
+        String VALID_ROLE_PATIENT = "Patient";
+        Person person = new PersonBuilder()
+                .withName(VALID_NAME_ALICE)
+                .withRole(VALID_ROLE_PATIENT)
+                .withAffiliations(VALID_NAME_ALICE)
+                .build();
+        AffiliationChecker affiliationChecker = new AffiliationChecker(person);
+        assertThrows(SamePersonAffiliationException.class, () -> affiliationChecker.check(model));
+    }
+
+    @Test
+    public void check_withAffiliationPersonSameRoleAsPersonAdding_SameRoleAffiliationException() {
+        String VALID_NAME_ALICE = "Alice Pauline";
+        String VALID_ROLE_DOCTOR = "Doctor";
+        Person person = new PersonBuilder()
+                .withName(VALID_NAME_BOB)
+                .withRole(VALID_ROLE_DOCTOR)
+                .withAffiliations(VALID_NAME_ALICE)
+                .build();
+        AffiliationChecker affiliationChecker = new AffiliationChecker(person);
+        assertThrows(SameRoleAffiliationException.class, () -> affiliationChecker.check(model));
+    }
+
+    @Test
+    public void check_validAffiliation_returnsTrue() {
+        String VALID_NAME_ALICE = "Alice Pauline";
+        String VALID_ROLE_PATIENT = "Patient";
+        Person person = new PersonBuilder()
+                .withName(VALID_NAME_BOB)
+                .withRole(VALID_ROLE_PATIENT)
+                .withAffiliations(VALID_NAME_ALICE)
+                .build();
+        AffiliationChecker affiliationChecker = new AffiliationChecker(person);
+        try {
+            assertTrue(affiliationChecker.check(model));
+        } catch (CommandException ce) {
+            fail();
+        }
+    }
+}
