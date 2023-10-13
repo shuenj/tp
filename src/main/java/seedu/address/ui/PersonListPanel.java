351fe2ab7c4e17cc932d116c101e1952317813c6
Seonlo99 <Zheng_chenglong1999@hotmail.com>
2023-10-13 17:17:20 +0800

    Remove unnecessary code

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -37,9 +37,6 @@ public class PersonListPanel extends UiPart<Region> {
         @Override
         protected void updateItem(Person person, boolean empty) {
             super.updateItem(person, empty);
-            for (Affiliation affiliation: person.getAffiliations()) {
-
-            }
 
             if (empty || person == null) {
                 setGraphic(null);
