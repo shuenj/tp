30973b00e3dc46448a14085c95f1af0c50e7cc2f
shuenj <60507123+shuenj@users.noreply.github.com>
2023-10-15 12:35:40 +0800

    Modify AddCommand to add affiliation to history

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -61,7 +61,7 @@ public class AddCommand extends Command {
 
         AuthenticateAffiliation.check(toAdd, model);
         AffiliationModifier.addAffiliations(toAdd.getAffiliations(), toAdd, model);
-        AffiliationModifier.addAffiliations(toAdd.getAffiliationHistory(), toAdd, model);
+        AffiliationModifier.addAffiliationHistory(toAdd.getAffiliations(), toAdd, model);
 
         model.addPerson(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
