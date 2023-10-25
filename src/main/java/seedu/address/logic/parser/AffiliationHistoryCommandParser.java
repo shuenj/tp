043b8bc2e6da37fde2a197c7772923a0fda95ea7
Seonlo99 <Zheng_chenglong1999@hotmail.com>
2023-10-25 21:38:38 +0800

    Add logger

--- src/main/java/seedu/address/logic/parser/AffiliationHistoryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AffiliationHistoryCommandParser.java
@@ -3,14 +3,19 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AffiliationHistoryCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.logging.Logger;
 
 /**
  * Parses input arguments and creates a new AffiliationCommand object
  */
 public class AffiliationHistoryCommandParser implements Parser<AffiliationHistoryCommand> {
+
+    private static final Logger logger = LogsCenter.getLogger(AddressBookParser.class);
 
     /**
      * Parses the given {@code String} of arguments in the context of the AffiliationCommand
@@ -24,6 +29,7 @@ public class AffiliationHistoryCommandParser implements Parser<AffiliationHistor
             Index index = ParserUtil.parseIndex(args);
             return new AffiliationHistoryCommand(index);
         } catch (ParseException pe) {
+            logger.finer("This user input caused a ParseException: " + args);
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AffiliationHistoryCommand.MESSAGE_USAGE), pe);
         }
