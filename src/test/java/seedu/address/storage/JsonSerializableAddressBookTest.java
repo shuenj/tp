e0ee0a30d914c1996ba87caa16e35d5fcae9d68d
leezhanpeng <zhanpenglee@outlook.com>
2023-11-01 04:24:43 +0800

    Add test cases

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -17,16 +17,25 @@ public class JsonSerializableAddressBookTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
     private static final Path TYPICAL_MIXED_FILE = TEST_DATA_FOLDER.resolve("typicalMixedAddressBook.json");
+    private static final Path INVALID_MIXED_FILE = TEST_DATA_FOLDER.resolve("invalidMixedAddressBook.json");
+
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
 
     @Test
-    public void toModelType_typicalPersonsFile_success() throws Exception {
+    public void toModelType_typicalMixedFile_success() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_MIXED_FILE,
                 JsonSerializableAddressBook.class).get();
         AddressBook addressBookFromFile = dataFromFile.toModelType();
         AddressBook typicalMixedAddressBook = TypicalMixed.getTypicalMixedAddressBook();
         assertEquals(addressBookFromFile, typicalMixedAddressBook);
+    }
+
+    @Test
+    public void toModelType_typicalMixFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_MIXED_FILE,
+                JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
 
     @Test
